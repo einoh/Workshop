ee523f1da34ac4d88e9eb77a545730e96ae8e8e8
Nicanor B. Nunez IV <34589681+einoh@users.noreply.github.com>
2018-01-12 21:49:00 +0800

    Update login.php

--- login.php
+++ login.php
@@ -41,14 +41,16 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6">
-          <h1>Welcome to Autos Database Web App</h1>
+          <center>
+            <h1>Welcome to </br>Autos Database </br>Web Application</h1>
+          </center
         </div>
         </br></br>
         <div class="col-md-6">
-          <div class="jumbotron" style="box-shadow: 0 4px 6px 1px rgba(50, 50, 50, 0.14);box-sizing: border-box;">
+          <div class="jumbotron" style="box-shadow: 0 4px 6px 1px rgba(50, 50, 50, 0.14);box-sizing: border-box; max-width: 330px;">
             <form class="form-signin" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-              <input type="text" class="form-control" placeholder="Email" autofocus name="username" style="margin-bottom: 10px;">
-              <input type="password" class="form-control" placeholder="Password" name="password">
+              <input type="text" class="form-control" placeholder="Email Address" autofocus name="username" style="margin-bottom: 10px;">
+              <input type="password" class="form-control" placeholder="Password" name="password" style="margin-bottom: 10px;">
               <input type="submit" class="btn btn-lg btn-primary btn-block" value="Sign In" />
               </br>
               <?php if(!empty(($_SESSION['error']))): ?>
